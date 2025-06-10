2ced86a6fd58b0e0650a29d91351679341e902fc
Ruben Cordero Muñoz <4517745098@edu.gencat.cat>
2025-06-10 11:58:34 +0200

    FinalVersion

---
+++ src/languages/ui.ts
@@ -0,0 +1,34 @@
+export const languages = {
+    mg: "Malagasy",
+    fr: "Français",
+}
+
+export const defaultLang = 'mg'
+
+export const ui = {
+    mg: {
+        "title": "Salama daholo, Tongasoa eto Mahajanga",
+        "descriptionOne": "Manana trano ngeza ampanofahina izahay, Ho an'izay mbola mitady trano madio be, 4 chambres, véranda fermé malalaka be, salon misy télé canal sat sy salle à manger ary ny cuisine misy ny rehetra azo andrahoana tsara fatana gaz ngeza be, toilette sy douche ao anatiny ary misy ihany koa any ivelany, izahay. Tompony mivantana azo atokisana tsara, trano malalaka be ho an i Fianakaviana mahazaka olona 20 Personnes eo, rano bobaka be (forage) chāteau d eau jiro misy foana mazava tsara, sécurisé, misy parking sady afaka hanovana grillade party.",
+        "descriptionTwo": "Amin'ny Facebook dia mandehana amin'ny “Moura Shop” ho an'izay liana rehetra",
+        "titleUbication": "Lalana mankany amin ilay Appartement",
+        "descriptionUbication": "Sady tsy lavitra ville no tsy lavitra plage ihany koa, route d Amborovy misy lalana mitsofoka kely, 5 mn Aqualand, Pizzeria, glace, 3 min à pied tsena kely misy trondro frais sy legumes sns ihany koa.",
+        "photosTitle": "Saripika sary"
+    },
+    fr: {
+        "title": "Bonjour à tous, Bienvenue à Mahajanga",
+        "descriptionOne": "Nous avons une petite maison à louer, pour ceux qui recherchent encore une maison très propre, 4 chambres, une spacieuse véranda fermée, un salon avec TV satellite et salle à manger et une cuisine avec tout ce qu'on peut bien cuisiner, une grande cuisinière à gaz, toilettes et douche intérieure et extérieure. Propriétaire direct de confiance, maison spacieuse pour une famille de 20 personnes, point d'eau (fourrage) en abondance, château d'eau, lumineux, toujours lumineux, sécurisé, parking et modulable pour grillade.",
+        "descriptionTwo": "Sur Facebook rendez-vous sur “Moura Shop” pour tous ceux que cela intéresse ",
+        "titleUbication": "Chemin vers l'appartement",
+        "descriptionUbication": "C'est à la fois pas loin de la ville et pas loin de la plage, route d'Amborovy avec une petite route, 5 minutes d'Aqualand, Pizzeria, glace, 3 minutes à pied du petit marché avec du poisson frais et des légumes, etc.",
+        "photosTitle": "Galerie de photos"
+
+    },
+    de: {
+        "title": "Hallo zusammen, Willkommen in Mahajanga",
+        "descriptionOne": "Wir haben ein kleines Haus zu vermieten, für diejenigen, die noch ein sehr sauberes Haus suchen, 4 Schlafzimmer, eine geräumige, geschlossene Veranda, Wohnzimmer mit Satellitenfernsehen und Esszimmer und eine Küche mit allem, was man gut kochen kann, großer Gasherd, Toilette und Innen- und Außendusche. Vertrauenswürdiger Direktvermieter, geräumiges Haus für eine 20-köpfige Familie, Wasserstelle (Futter) im Überfluss, Wasserturm, hell, immer hell, sicher, Parkplatz und modulierbar zum Grillen.",
+        "descriptionTwo": "Auf Facebook gehen Sie zu “Moura Shop” für alle, die interessiert sind",
+        "titleUbication": "Weg zur Wohnung",
+        "descriptionUbication": "Es ist sowohl nicht weit von der Stadt als auch nicht weit vom Strand entfernt, Route d'Amborovy mit einer kleinen Straße, 5 Minuten von Aqualand, Pizzeria, Eis, 3 Minuten zu Fuß zum kleinen Markt mit frischem Fisch und Gemüse usw.",
+        "photosTitle": "Fotogalerie"
+    },
+} as const
